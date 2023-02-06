b01487b3cd8f47ed1a9748e1c21403d0022a37fd
gron1gh1 <sgkang@brandazine.com>
2023-02-06 10:05:37 +0900

    chore: esbuild external default packages

--- esbuild.config.js
+++ esbuild.config.js
@@ -3,6 +3,7 @@ import { build } from "esbuild";
 build({
   entryPoints: ["./src/index.js"],
   bundle: true,
+  external: ["fs", "path", "constants", "stream", "assert", "util"],
   format: "esm",
   outdir: "./bin",
 });
