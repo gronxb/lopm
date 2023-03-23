33eef31ef3814056d6429ed5b9cad9ba6ae31e0c
gron1gh1 <gron1gh1@gmail.com>
2023-03-23 17:15:35 +0900

    fix: jest timeout

--- src/build.spec.ts
+++ src/build.spec.ts
@@ -2,7 +2,7 @@ import { exec } from "child_process";
 import { join } from "path";
 import { version } from "../package.json";
 
-jest.setTimeout(10000);
+jest.setTimeout(60000);
 
 test("should print package.json version to stdout", (done) => {
   const command = ["node", join("bin", "index.cjs"), "-v"].join(" ");
