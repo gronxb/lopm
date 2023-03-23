e4ec6de722ab3ee50b4793ee48618811b4e7bafd
gron1gh1 <gron1gh1@gmail.com>
2023-03-23 17:19:50 +0900

    fix: turbo test

--- src/build.spec.ts
+++ src/build.spec.ts
@@ -2,7 +2,7 @@ import { exec } from "child_process";
 import { join } from "path";
 import { version } from "../package.json";
 
-jest.setTimeout(60000);
+jest.setTimeout(10000);
 
 test("should print package.json version to stdout", (done) => {
   const command = ["node", join("bin", "index.cjs"), "-v"].join(" ");
