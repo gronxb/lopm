c227d768c28790d311acb1b6e658c4368161a896
Sungyu Kang <gron1gh1@gmail.com>
2023-03-23 17:21:02 +0900

    chore: build test and integration (#6)
    * chore(turbo): using turborepo
    
    * test(build): version check
    
    * chore(action): integration
    
    * fix: jest timeout
    
    * fix: turbo test

---
+++ src/test/utils/execCommand.ts
@@ -0,0 +1,47 @@
+import { ChildProcess, spawn } from "child_process";
+
+import EventEmitter from "events";
+import { Readable } from "stream";
+
+export const execCommand = async (
+  command: string,
+  workDir: string
+): Promise<{ stdout: string; stderr: string; code: number }> => {
+  return new Promise((resolve, reject) => {
+    const childProcess = spawn(command, {
+      shell: true,
+      cwd: workDir,
+    });
+
+    let stdout = "";
+    childProcess.stdout.on("data", (data) => {
+      stdout = stdout + data.toString();
+    });
+
+    let stderr = "";
+    childProcess.stderr.on("data", (data) => {
+      stderr = stderr + data.toString();
+    });
+
+    childProcess.on("close", (code) => {
+      if (code === 0) {
+        resolve({ stdout, stderr, code });
+      } else {
+        reject(new Error(stderr.trim()));
+      }
+    });
+
+    childProcess.on("error", (err) => {
+      reject(err);
+    });
+  });
+};
+
+export const mockChildProcess = (): ChildProcess => {
+  const proc = new EventEmitter() as ChildProcess;
+
+  proc.stdout = new EventEmitter() as Readable;
+  proc.stderr = new EventEmitter() as Readable;
+
+  return proc;
+};
